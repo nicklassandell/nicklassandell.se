ec28ab983e927b036c2fd1da9659384af8792c25
Nicklas Sandell <nike.sandell@gmail.com>
2014-06-28 13:48:45 +0200

    Added WebP support check

--- js/script.js
+++ js/script.js
@@ -6,4 +6,19 @@ jQuery(document).ready(function($) {
 
 	$("h1").fitText(1, { minFontSize: '45px', maxFontSize: '96px' });
 
+	// Test for WebP support (for bg img)
+	testWebP(function(support) {
+		if(support) {
+			$('body').addClass('webp');
+		}
+	});
+
+	function testWebP(callback) {
+		var webP = new Image();
+		webP.onload = webP.onerror = function () {
+			callback(webP.height == 2);
+		};
+		webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+	};
+
 });
