b0beefb7ebea3a73d7ae92e84aa0af56867b5849
Nicklas Sandell <nike.sandell@gmail.com>
2014-06-30 23:01:08 +0200

    Mail link bugfix. Added link to blog. Added icons for each social link.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 jQuery(document).ready(function($) {
 
 	// Add Email through Javascript to prevent scraping
-	$('.email-link').attr('href', 'nike.sandell@gmail.com').text('nike.sandell@gmail.com');
+	$('.email-link').attr('href', 'mailto:nike.sandell@gmail.com').html('<i class="ion-android-mail"></i> nike.sandell@gmail.com');
 
 	$("h1").fitText(1, { minFontSize: '45px', maxFontSize: '96px' });
 
