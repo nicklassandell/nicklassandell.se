ee71799cbe65d4a687ee0683a4a2f343826a2608
Nicklas Sandell <nike.sandell@gmail.com>
2014-07-01 13:22:57 +0200

    Ditched background image in favor of background color transition. Also removed icons.

--- js/script.js
+++ js/script.js
@@ -2,23 +2,56 @@
 jQuery(document).ready(function($) {
 
 	// Add Email through Javascript to prevent scraping
-	$('.email-link').attr('href', 'mailto:nike.sandell@gmail.com').html('<i class="ion-android-mail"></i> nike.sandell@gmail.com');
+	$('.email-link').attr('href', 'mailto:nike.sandell@gmail.com').html('nike.sandell@gmail.com');
 
+
+	// Header resizing
 	$("h1").fitText(1, { minFontSize: '45px', maxFontSize: '96px' });
 
-	// Test for WebP support (for bg img)
-	testWebP(function(support) {
-		if(support) {
-			$('body').addClass('webp');
-		}
-	});
 
-	function testWebP(callback) {
-		var webP = new Image();
-		webP.onload = webP.onerror = function () {
-			callback(webP.height == 2);
-		};
-		webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
-	};
+	var bgTarget = $('body'),
+		bgColors = ['#13223C', '#133C3A', '#2E411D', '#47231A', '#461A34', '#1E1A46'],
+		currBg = 0,
+		bgSwitchInterval = 3*1000;
+
+	// Add background transitioning if transition is supported
+	if(detectCSSFeature('transition')) {
+		switchBg();
+		setInterval(switchBg, bgSwitchInterval);
+	}
+
+	function switchBg() {
+
+		currBg++;
+		if(!bgColors[currBg]) {
+			currBg = 1;
+		}
+
+		bgTarget.css('background-color', bgColors[currBg]);
+
+		console.log('switching to', currBg, bgColors[currBg]);
+	}	
+
+	function detectCSSFeature(featurename){
+	    var feature = false,
+	    domPrefixes = 'Webkit Moz ms O'.split(' '),
+	    elm = document.createElement('div'),
+	    featurenameCapital = null;
+
+	    featurename = featurename.toLowerCase();
+
+	    if( elm.style[featurename] !== undefined) { feature = true; }
+
+	    if( feature === false ) {
+	        featurenameCapital = featurename.charAt(0).toUpperCase() + featurename.substr(1);
+	        for( var i = 0; i < domPrefixes.length; i++ ) {
+	            if( elm.style[domPrefixes[i] + featurenameCapital ] !== undefined ) {
+	              feature = true;
+	              break;
+	            }
+	        }
+	    }
+	    return feature; 
+	}
 
 });
